0645db82a56e83248ea5b9d5a6257fe611a4f18b
Eva Orlandini <evaorlandini.pro@gmail.com>
2024-11-22 17:22:41 +0800

    root added

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :lists, only: [:index, :show, :new, :create] do
+  root to: "lists#index"
+  resources :lists, only: [:show, :new, :create] do
     resources :bookmarks, only: [:new, :create, :destroy]
   end
 end
